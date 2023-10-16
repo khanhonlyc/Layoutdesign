c9ee712e114730ea4f00519f3e05263340576e3b
khanhonlyc <khanhonlyc@gmail.com>
2023-10-16 17:30:13 +0700

    slider-carousel

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,12 @@ $('.slider').slick({
     slidesToScroll: 1,
     autoplay: true,
     dots: true,
+    prevArrow:`<div class="icon-left"><svg width="8" class='slick-prev pull-left' height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg></div>`,
+    nextArrow:`<div class="icon-right"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M1 13L7 7L1 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg></div>`,
     draggable : false,
     autoplaySpeed: 5000,
   });
