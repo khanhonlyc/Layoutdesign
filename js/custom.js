500f158d8f42dfd7a3e749cdd5bc70e174ec6fe0
khanhonlyc <khanhonlyc@gmail.com>
2023-10-20 13:57:20 +0700

    main-top

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,8 @@
-// $('.main-slider').slick();
-// $('.slider').slick();
 $('.slider').slick({
-    // slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     dots: true,
+    // infinite: true,
     prevArrow:`<div class="icon-left"><svg width="8" class='slick-prev pull-left' height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M7 13L1 7L7 1" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg></div>`,
@@ -13,5 +11,20 @@ $('.slider').slick({
     </svg></div>`,
     draggable : false,
     autoplaySpeed: 5000,
+    // responsive: [
+    //   {
+    //     breakpoint: 1024,
+    //     settings: {
+    //       slidesToShow: 3
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2
+    //     }
+    //   }
+    // ]
   });
       
